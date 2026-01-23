de8f202398d5b808617e91e894f765e0a436b259
Gareth Healy <garethahealy@gmail.com>
2026-01-23 11:02:54 +0000

    split seed and file seed
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- src/main/java/com/garethahealy/gameoflife/seeds/Seed.java
+++ src/main/java/com/garethahealy/gameoflife/seeds/Seed.java
@@ -1,10 +1,6 @@
 package com.garethahealy.gameoflife.seeds;
 
-import com.garethahealy.gameoflife.model.GameBoard;
-
-import java.io.IOException;
-
 public interface Seed {
 
-    void process(GameBoard board) throws IOException;
+    SeedPattern process();
 }
