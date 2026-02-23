c038511db5fbfd8fce7f4e70adc21a006a847f59
Gareth Healy <garethahealy@gmail.com>
2026-02-23 12:44:42 +0000

    added dump startup test
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

---
+++ src/test/java/com/garethahealy/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,13 @@
+package com.garethahealy.gameoflife;
+
+import io.quarkus.test.junit.main.QuarkusMainTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusMainTest
+public class GameOfLifeTest {
+
+    @Test
+    void test() {
+        //dummy test
+    }
+}
